1f8b1c304730bdd5a66627a2dfaca953411f3e7b
mohamedbebba <mohamedbebba1@gmail.com>
2024-05-04 20:02:43 +0200

    init game

--- src/ziffernreihespiel/ErgebnislisteTest.java
+++ src/ziffernreihespiel/ErgebnislisteTest.java
@@ -7,7 +7,7 @@ public class ErgebnislisteTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Ergebnisliste ergebnisliste = new Ergebnisliste(); // Ergebnisliste erstellen
-        ergebnisliste.playGame();
+        ergebnisliste.playGame();// Init the game
 
         scanner.close();
     }
