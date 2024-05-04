3d9a443d8defafe64b7ac948907e828363a03741
mohamedbebba <mohamedbebba1@gmail.com>
2024-05-04 20:51:47 +0200

    format code

--- src/ziffernreihespiel/ErgebnislisteTest.java
+++ src/ziffernreihespiel/ErgebnislisteTest.java
@@ -1,14 +1,10 @@
 package ziffernreihespiel;
 
-import java.util.Scanner;
-
 public class ErgebnislisteTest {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         Ergebnisliste ergebnisliste = new Ergebnisliste(); // Ergebnisliste erstellen
         ergebnisliste.playGame();// Init the game
 
-        scanner.close();
     }
 }
