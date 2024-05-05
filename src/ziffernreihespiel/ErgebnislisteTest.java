afd5a38cb6e1feb82c02941ced7a1386bffbd480
Mohamed Bebba <91488872+MoBebba123@users.noreply.github.com>
2024-05-05 11:14:22 +0200

    game test

--- src/ziffernreihespiel/ErgebnislisteTest.java
+++ src/ziffernreihespiel/ErgebnislisteTest.java
@@ -4,7 +4,7 @@ public class ErgebnislisteTest {
 
     public static void main(String[] args) {
         Ergebnisliste ergebnisliste = new Ergebnisliste(); // Ergebnisliste erstellen
-        ergebnisliste.playGame();// Init the game
+        ergebnisliste.playGame(); // Init the game
 
     }
 }
